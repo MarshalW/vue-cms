3b3f08336e0560f4428c8590c39af464d84c5528
Marshal <marshal.wu@qq.com>
2018-05-22 16:50:41 +0800

    形成基本的编辑器布局

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,12 @@ export default {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
+        background-color: beige;
         margin-top: 60px;
+
+        margin-left: auto;
+        margin-right: auto;
+        width: 600px;
+        min-height: 750px;
     }
 </style>
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Editor</div>
+    <div class="editorContainer">
+        <div class="editPannel">edit pannel</div>
+        <div class="componentsPannel">components pannel</div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +12,24 @@ export default {
 </script>
 
 <style scoped>
+    .editorContainer {
+        display: flex;
+        flex-direction: row;
+        min-height: inherit;
+    }
 
+    .editorContainer div {
+        margin: 10px;
+        min-height: inherit;
+    }
+
+    .editPannel {
+        width: 350px;
+        background-color: cornsilk;
+    }
+
+    .componentsPannel {
+        background-color: aliceblue;
+        flex: 1 30%;
+    }
 </style>
